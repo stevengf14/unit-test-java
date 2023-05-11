306701c96574bad68c3d7eee2f0c94844eb60db5
Steven <stevengf14@gmail.com>
2023-05-10 23:19:41 -0500

    Calculator class

---
+++ src/main/java/ec/com/learning/unittest/junit5/Calculator.java
@@ -0,0 +1,19 @@
+package ec.com.learning.unittest.junit5;
+
+/**
+ *
+ * @author Steven Guamán - May 2023
+ */
+public class Calculator {
+
+    private int result;
+
+    public int add(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public int subtrat(int number1, int number2) {
+        return number1 - number2;
+    }
+    
+}
