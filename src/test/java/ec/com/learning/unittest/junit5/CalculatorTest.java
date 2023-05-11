a17e95177faf96f0ed0298e356d6eb776521a1c3
Steven <stevengf14@gmail.com>
2023-05-10 23:46:42 -0500

    @Test tag

--- src/test/java/ec/com/learning/unittest/junit5/CalculatorTest.java
+++ src/test/java/ec/com/learning/unittest/junit5/CalculatorTest.java
@@ -1,10 +1,22 @@
 package ec.com.learning.unittest.junit5;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import org.junit.jupiter.api.Test;
+
 /**
  *
  * @author Steven Guamán - May 2023
  */
 public class CalculatorTest {
-    
-    
+
+    private Calculator calculator;
+
+    @Test
+    public void calculatorNotNullTest() {
+        //assertNull(calculator);
+        calculator = new Calculator();
+        assertNotNull(calculator, "Calculator must be not null");
+    }
+
 }
