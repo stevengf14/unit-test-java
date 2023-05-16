1a37a2814b04b75bc7460ead76b72dcb2c27dc40
Steven <stevengf14@gmail.com>
2023-05-15 21:36:22 -0500

    new methods

--- src/main/java/ec/com/learning/unittest/junit5/Calculator.java
+++ src/main/java/ec/com/learning/unittest/junit5/Calculator.java
@@ -15,5 +15,16 @@ public class Calculator {
     public int subtract(int number1, int number2) {
         return number1 - number2;
     }
-    
+
+    public int divide(int number1, int number2) {
+        return number1 / number2;
+    }
+
+    public int divideByZero(int number1, int number2) {
+        if (number2 == 0) {
+            throw new ArithmeticException("No se puede dividir por cero");
+        }
+        return number1 / number2;
+    }
+
 }
