54b1406603c2682b59ea0dc11816157ea65b0cc0
Steven <stevengf14@gmail.com>
2023-05-17 22:42:50 -0500

    cleaning code

--- src/test/java/ec/com/learning/unittest/junit5/TemperatureCalculatorTest.java
+++ src/test/java/ec/com/learning/unittest/junit5/TemperatureCalculatorTest.java
@@ -1,8 +1,6 @@
 package ec.com.learning.unittest.junit5;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
