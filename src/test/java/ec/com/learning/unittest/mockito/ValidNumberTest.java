781d4e271a1de340daf80083b0dbbd702d841dcd
Steven <stevengf14@gmail.com>
2023-05-19 23:35:00 -0500

    Testing ValidNumber

---
+++ src/test/java/ec/com/learning/unittest/mockito/ValidNumberTest.java
@@ -0,0 +1,41 @@
+package ec.com.learning.unittest.mockito;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Steven Guamán - May 2023
+ */
+public class ValidNumberTest {
+
+    private ValidNumber validNumber;
+
+    @BeforeEach
+    public void setUp() {
+        validNumber = new ValidNumber();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        validNumber = null;
+    }
+
+    @Test
+    public void checkTest() {
+        assertEquals(true, validNumber.check(5));
+    }
+
+    @Test
+    public void checkNegativeTest() {
+        assertEquals(false, validNumber.check(-5));
+    }
+
+    @Test
+    public void checkStringTest() {
+        assertEquals(false, validNumber.check("five"));
+    }
+    
+}
