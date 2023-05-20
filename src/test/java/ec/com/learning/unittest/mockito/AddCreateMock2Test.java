444ed34c6565130702e38fe5f9ba8c50e39e48e0
Steven <stevengf14@gmail.com>
2023-05-19 23:55:11 -0500

    Creating Mock with Mockito Annotations

--- src/test/java/ec/com/learning/unittest/mockito/AddCreateMock2Test.java
+++ src/test/java/ec/com/learning/unittest/mockito/AddCreateMock2Test.java
@@ -2,28 +2,30 @@ package ec.com.learning.unittest.mockito;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 /**
  *
  * @author Steven Guamán - May 2023
  */
-public class AddCreateMock1Test {
+public class AddCreateMock2Test {
 
+    @InjectMocks
     private Add add;
+    @Mock
     private ValidNumber validNumber;
 
     @BeforeEach
     public void setUp() {
-        validNumber = Mockito.mock(ValidNumber.class);
-        add = new Add(validNumber);
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
     public void addTest() {
         add.add(3, 2);
         Mockito.verify(validNumber).check(3);
-        //Mockito.verify(validNumber).check(5);
     }
-
 }
