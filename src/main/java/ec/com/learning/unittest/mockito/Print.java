882efe610f8c91c8200e66590e232cff37df8449
Steven <stevengf14@gmail.com>
2023-05-24 23:39:30 -0500

    Print class

---
+++ src/main/java/ec/com/learning/unittest/mockito/Print.java
@@ -0,0 +1,17 @@
+package ec.com.learning.unittest.mockito;
+
+/**
+ *
+ * @author Steven Guamán - May 2023
+ */
+public class Print {
+
+    public void showMessage(int number) {
+
+    }
+
+    public void showError() {
+
+    }
+
+}
